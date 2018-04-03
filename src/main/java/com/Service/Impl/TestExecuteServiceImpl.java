e54bb2c77aae8066ec8de46d3d9fe0665687f60c
chiyo <cinro9@gmail.com>
2018-04-03 15:00:18 +0800

    use sh

--- src/main/java/com/Service/Impl/TestExecuteServiceImpl.java
+++ src/main/java/com/Service/Impl/TestExecuteServiceImpl.java
@@ -30,14 +30,33 @@ public class TestExecuteServiceImpl implements TestExecuteService{
     @Autowired
     private TestRepository testRepository;
 
+
+
     @Override
     public ReportVO javaTestAll(Long testId ,String username) {
         Runtime runtime = Runtime.getRuntime();
         String command="";
         String src=testRepository.findById(testId).getSrc();
-        String disk=src.split(":")[0];
 
-        command="cmd /c  "+disk+": && cd "+src+" && call mvn test ";
+        String path=""+src;
+        String shPath=src+="/exectest.sh";
+        File shfile = new File(path);
+        if(!shfile.exists()){
+            shfile.getParentFile().mkdirs();
+        }
+        try {
+            shfile.createNewFile();
+            FileWriter fw = new FileWriter(shfile, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(ScriptServiceImpl.javashAll());
+            bw.flush();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        command="cd "+path+" && sh ./exectest.sh ";
 
         String line = null;
         String out="";
@@ -72,14 +91,26 @@ public class TestExecuteServiceImpl implements TestExecuteService{
         Runtime runtime = Runtime.getRuntime();
         String command="";
         String src=testRepository.findById(testId).getSrc();
-        String disk=src.split(":")[0];
 
-        command="cmd /c "+disk+": && cd "+src+" && call mvn -Dtest=";
-        for(String s:file){
-            command=command+s+",";
+        String path=""+src;
+        String shPath=src+="/exectest.sh";
+        File shfile = new File(path);
+        if(!shfile.exists()){
+            shfile.getParentFile().mkdirs();
         }
-        command=command.substring(0,command.lastIndexOf(','));
-        command+=" test";
+        try {
+            shfile.createNewFile();
+            FileWriter fw = new FileWriter(shfile, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(ScriptServiceImpl.javash(file));
+            bw.flush();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        command="cd "+path+" && sh ./exectest.sh ";
 
         String line = null;
         String out="";
@@ -114,13 +145,26 @@ public class TestExecuteServiceImpl implements TestExecuteService{
         Runtime runtime = Runtime.getRuntime();
         String command="";
         String src=testRepository.findById(testId).getSrc();
-        String disk=src.split(":")[0];
 
-        command="cmd /c "+disk+": && cd "+src+" &&  py.test ";
-        for(String s:file){
-            command=command+s+" ";
+        String path=""+src;
+        String shPath=src+="/exectest.sh";
+        File shfile = new File(path);
+        if(!shfile.exists()){
+            shfile.getParentFile().mkdirs();
         }
-        command+=" --junitxml="+src+"\\report\\log.xml";
+        try {
+            shfile.createNewFile();
+            FileWriter fw = new FileWriter(shfile, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(ScriptServiceImpl.pythonsh(file));
+            bw.flush();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        command="cd "+path+" && sh ./exectest.sh ";
 
         String line = null;
         String out="";
@@ -178,12 +222,12 @@ public class TestExecuteServiceImpl implements TestExecuteService{
     }
 
     @Override
-    public ReportVO cTest(List<String> file, Long testId ,String username) {  //需要报告文件名
+    public ReportVO cTest(List<String> file, Long testId ,String username) {
         Runtime runtime = Runtime.getRuntime();
         String command="";
         String src=testRepository.findById(testId).getSrc();
 
-        String path=src+="\\makefile";
+        String path=src+="/makefile";
         File makefile = new File(path);
         if(!makefile.exists()){
             makefile.getParentFile().mkdirs();
@@ -192,13 +236,8 @@ public class TestExecuteServiceImpl implements TestExecuteService{
             makefile.createNewFile();
             FileWriter fw = new FileWriter(makefile, false);
             BufferedWriter bw = new BufferedWriter(fw);
-            String content="IINC=-I/usr/local/include/CUnit\n" +
-                    "LIB=-L/usr/local/lib/\n\n"+"all: ";
-            for(String str:file){
-                content+=str+" ";
-            }
-            content=content+"\n"+"\tgcc $^ -o test $(INC) $(LIB) -lcunit -static ";
-            bw.write(content);
+
+            bw.write(ScriptServiceImpl.cmakefile(file));
             bw.flush();
             bw.close();
             fw.close();
@@ -206,7 +245,24 @@ public class TestExecuteServiceImpl implements TestExecuteService{
             e.printStackTrace();
         }
 
-        command="make&& ./test "; //to do
+        String shPath=src+="/exectest.sh";
+        File shfile = new File(shPath);
+        if(!shfile.exists()){
+            shfile.getParentFile().mkdirs();
+        }
+        try {
+            shfile.createNewFile();
+            FileWriter fw = new FileWriter(shfile, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(ScriptServiceImpl.csh(src));
+            bw.flush();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        command="cd "+path+" && sh ./exectest.sh ";
 
         String line = null;
         String out="";
@@ -233,23 +289,14 @@ public class TestExecuteServiceImpl implements TestExecuteService{
     }
 
     private Report javaReport(String src){
-        src+="\\target\\surefire-reports";
+        src+="/target/surefire-reports";
         File dir=new File(src);
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        Report report=new Report();
-        Date d = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        report.setTime(sdf.format(d));
-
-        int cases=0;
-        int failures=0;
-        int success=0;
-        int error=0;
 
         if (!dir.isDirectory()) {
             System.out.println("not a dir");
-
+            Report report=new Report();
             report.setError_info("diretory error");
+            return report;
         } else {
             // 内部匿名类，用来过滤文件类型
             File[] xmlList = dir.listFiles(new FileFilter() {
@@ -261,154 +308,21 @@ public class TestExecuteServiceImpl implements TestExecuteService{
                     }
                 }
             });
-            for (File f:xmlList) {
-                DocumentBuilder builder = null;
-                try {
-                    builder = factory.newDocumentBuilder();
-                    Document document = builder.parse(f);
-                    Element rootElement = document.getDocumentElement();
-                    cases+=Integer.parseInt(rootElement.getAttribute("tests"));
-                    failures+=Integer.parseInt(rootElement.getAttribute("failures"));
-                    int failnum=Integer.parseInt(rootElement.getAttribute("failures"));
-                    String name=rootElement.getAttribute("name");
-                    error=Integer.parseInt(rootElement.getAttribute("errors"));
-
-                    if(failnum>0){
-                        NodeList failList = rootElement.getElementsByTagName("failure");
-                        for(int i=0;i<failnum;i++){
-                            Element fail = (Element)failList.item(i);
-                            FaultInfo faultInfo=new FaultInfo();
-                            faultInfo.setCase_name(name);
-                            String funcname=((Element)fail.getParentNode()).getAttribute("name");
-                            faultInfo.setFunc_name(funcname);
-                            String type=fail.getAttribute("type");
-                            if(fail.getAttribute("message")!=null){
-                                type=type+"  "+fail.getAttribute("message");
-                            }
-                            faultInfo.setType(type);
-                            String value=fail.getFirstChild().getNodeValue();
-                            int line=0;
-                            int start=value.indexOf(funcname);
-                            int end=value.indexOf(")",start);
-                            value=value.substring(start,end);
-                            value=value.split(":")[1];
-                            line=Integer.parseInt(value);
-                            faultInfo.setLine(line);
-                            report.addFault_info(faultInfo);
-
-                            System.out.println(name+" "+funcname+" "+line+" "+type);
-
-                        }
-
-
-                    }
-
-                } catch (ParserConfigurationException e) {
-                    e.printStackTrace();
-                }catch (SAXException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-            success=cases-failures;
-            report.setCase_num(cases);
-            report.setFail_num(failures);
-            report.setSucess_num(success);
-            report.setError_info("");
-            if(cases==0){
-                report.setError_info("result do not exist");
-            }
+            return ReportGenerateImpl.javaXmlReport(xmlList);
 
         }
 
-        return report;
+
     }
 
     private Report pythonReport(String src){
-        src+="\\report\\log.xml";
+        src+="/log.xml";
         File log=new File(src);
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        Report report=new Report();
-        Date d = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        report.setTime(sdf.format(d));
-
-        int cases=0;
-        int failures=0;
-        int success=0;
-        int error=0;
-
-        DocumentBuilder builder = null;
-
-        try {
-            builder = factory.newDocumentBuilder();
-            Document document = builder.parse(log);
-            Element rootElement = document.getDocumentElement();
-            cases=Integer.parseInt(rootElement.getAttribute("tests"));
-            failures=Integer.parseInt(rootElement.getAttribute("failures"));
-            int failnum=Integer.parseInt(rootElement.getAttribute("failures"));
-            error=Integer.parseInt(rootElement.getAttribute("errors"));
-
-            if(failnum>0){
-                NodeList failList = rootElement.getElementsByTagName("testcase");
-                for(int i=0;i<failList.getLength();i++){
-                    Element fail = (Element)failList.item(i);
-                    FaultInfo faultInfo=new FaultInfo();
-                    String name=fail.getAttribute("file");
-                    faultInfo.setCase_name(name);
-                    String funcname=fail.getAttribute("name");
-                    faultInfo.setFunc_name(funcname);
-                    int line=Integer.parseInt(fail.getAttribute("line"));
-                    faultInfo.setLine(line);
-                    Element failure=(Element) fail.getFirstChild();
-                    if(failure!=null){
-                        String type=failure.getAttribute("message");
-                        faultInfo.setType(type);
-                        report.addFault_info(faultInfo);
-
-                        System.out.println(name+" "+funcname+" "+line+" "+type);
-                    }
-
-                }
-
-            }
-            if(error>0){
-                NodeList errorList = rootElement.getElementsByTagName("error");
-                Element errorTemp=(Element) errorList.item(0);
-                String errorInfo=errorTemp.getAttribute("message");
-                errorInfo+="\n"+errorTemp.getFirstChild().getNodeValue();
-                report.setError_info(errorInfo);
-            }
-
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    success=cases-failures;
-    report.setCase_num(cases);
-    report.setFail_num(failures);
-    report.setSucess_num(success);
-    report.setError_info("");
-    if(cases==0) {
-        report.setError_info("result do not exist");
+        return ReportGenerateImpl.pythonXmlReport(log);
     }
 
-        return report;
-    }
-
-    private Report cReport(String src){  //需要dtd
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    private Report cReport(String src){
         Report report=new Report();
-        Date d = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        report.setTime(sdf.format(d));
         File log=null;
         File dir=new File(src);
         if (!dir.isDirectory()) {
@@ -428,79 +342,10 @@ public class TestExecuteServiceImpl implements TestExecuteService{
             });
 
             log=xmlList[0];
+            report=ReportGenerateImpl.cXmlReport(log);
         }
-
         if(log==null){
             report.setError_info("no file");
-            return report;
-        }
-
-        int cases=0;
-        int failures=0;
-        int success=0;
-        int error=0;
-
-        DocumentBuilder builder = null;
-
-        try {
-            builder = factory.newDocumentBuilder();
-            Document document = builder.parse(log);
-            Element rootElement = document.getDocumentElement();
-
-            NodeList sumList = rootElement.getElementsByTagName("CUNIT_RUN_SUMMARY_RECORD");
-            Element caseSum = (Element)sumList.item(1);
-            String temp=caseSum.getElementsByTagName("RUN").item(0).getFirstChild().getNodeValue().trim();
-            System.out.println(temp);
-            cases=Integer.parseInt(temp);
-            temp=caseSum.getElementsByTagName("FAILED").item(0).getFirstChild().getNodeValue().trim();
-            failures=Integer.parseInt(temp);
-
-            if(failures>0){
-                NodeList failList = rootElement.getElementsByTagName("CUNIT_RUN_TEST_FAILURE");
-                for(int i=0;i<failList.getLength();i++){
-                    Element fail = (Element)failList.item(i);
-                    FaultInfo faultInfo=new FaultInfo();
-                    String name=fail.getElementsByTagName("FILE_NAME").item(0).getFirstChild().getNodeValue().trim();
-                    faultInfo.setCase_name(name);
-                    String funcname=fail.getElementsByTagName("TEST_NAME").item(0).getFirstChild().getNodeValue().trim();
-                    faultInfo.setFunc_name(funcname);
-                    int line=Integer.parseInt(fail.getElementsByTagName("LINE_NUMBER").item(0).getFirstChild().getNodeValue().trim());
-                    faultInfo.setLine(line);
-                    String failure=fail.getElementsByTagName("CONDITION").item(0).getFirstChild().getNodeValue().trim();
-                    if(failure!=null){
-                        faultInfo.setType(failure);
-                        report.addFault_info(faultInfo);
-
-                        System.out.println(name+" "+funcname+" "+line+" "+failure);
-                    }
-
-                }
-
-            }
-            if(error>0){
-                NodeList errorList = rootElement.getElementsByTagName("error");
-                Element errorTemp=(Element) errorList.item(0);
-                String errorInfo=errorTemp.getAttribute("message");
-                errorInfo+="\n"+errorTemp.getFirstChild().getNodeValue();
-                report.setError_info(errorInfo);
-            }
-
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        success=cases-failures;
-        report.setCase_num(cases);
-        report.setFail_num(failures);
-        report.setSucess_num(success);
-        report.setError_info("");
-        if(cases==0) {
-            report.setError_info("result do not exist");
         }
 
         return report;
