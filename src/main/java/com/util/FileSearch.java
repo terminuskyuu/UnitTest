01dc046c4653c936c28cbe471321082dbb2f42e4
chiyo <cinro9@gmail.com>
2018-04-16 15:14:35 +0800

    file recurit search

---
+++ src/main/java/com/util/FileSearch.java
@@ -0,0 +1,51 @@
+package com.util;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSearch {
+    public static void main(String[] args){
+        String s="C:\\java\\test";
+        List<String> fileList=getAllFiles(".py","",s,"");
+        for(String temp:fileList){
+            System.out.println(temp);
+        }
+
+    }
+
+
+    private static List<String> getAllFiles(String postfix, String content, String path,String pre){
+        List<String> fileList=new ArrayList<String>();
+        File dir=new File(path);
+        File[] allList = dir.listFiles();
+        for(File f:allList){
+            if(f.isDirectory()){
+                if(pre!=null&&!pre.equals("")){
+                    List<String> tempList=getAllFiles(postfix,content,f.getPath(),pre+"/"+f.getName());
+                    for(String s:tempList){
+                        fileList.add(s);
+                    }
+                }else{
+                    List<String> tempList=getAllFiles(postfix,content,f.getPath(),f.getName());
+                    for(String s:tempList){
+                        fileList.add(s);
+                    }
+                }
+
+            }
+
+            if(f.isFile() && f.getName().endsWith(postfix)&&f.getName().contains(content)){
+                if(pre!=null&&!pre.equals("")){
+                    fileList.add(pre+"/"+f.getName());
+                }else{
+                    fileList.add(f.getName());
+                }
+
+            }
+
+        }
+        return fileList;
+    }
+
+}
